5b7d01157fa418547717b20cb4b0b5b9f87b0852
Daniel Ma <drailskid@yahoo.com>
2015-12-15 07:23:26 -0800

    parse login

---
+++ src/containers/App/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ParseReact from 'parse-react'
+import { User } from 'parse'
+import { Main, Login } from 'containers'
+
+export default React.createClass({
+  mixins: [ParseReact.Mixin],
+
+  observe() {
+    return { user: ParseReact.currentUser }
+  },
+
+  render() {
+    if (this.data.user) {
+      return (
+        <div>
+          <a className="logOut" onClick={User.logOut}>
+            <svg viewBox="0 0 60 60">
+              <path d="M0,0 L30,0 L30,10 L10,10 L10,50 L30,50 L30,60 L0,60 Z"></path>
+              <path d="M20,23 L40,23 L40,10 L60,30 L40,50 L40,37 L20,37 Z"></path>
+            </svg>
+          </a>
+          <Main />
+        </div>
+      )
+    }
+
+    return <Login />
+  },
+})
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Parse from 'parse'
 import { render } from 'react-dom';
-import { App } from 'components';
+import { App } from 'containers';
+
+Parse.initialize(
+  'mBMPw6wrZ1sWdo5C0I3fjh9JYetqOaQXbrKbjiXE',
+  'msIKCvhGCUQCKO3zHjvh3vuYoS9QnI12qEuUySj1'
+)
 
 render(<App />, document.getElementById('root'));
