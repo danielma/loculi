948208034544064cfc02bdf975b9f568c9df3326
Daniel Ma <drailskid@yahoo.com>
2015-12-15 23:31:43 -0800

    add style

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -11,20 +11,22 @@ export default React.createClass({
   },
 
   render() {
-    if (this.data.user) {
-      return (
-        <div>
-          <a className="logOut" onClick={User.logOut}>
-            <svg viewBox="0 0 60 60">
-              <path d="M0,0 L30,0 L30,10 L10,10 L10,50 L30,50 L30,60 L0,60 Z"></path>
-              <path d="M20,23 L40,23 L40,10 L60,30 L40,50 L40,37 L20,37 Z"></path>
-            </svg>
-          </a>
-          <Main />
-        </div>
-      )
-    }
+    const styles = require('./App.sass')
 
-    return <Login />
+    return (
+      <div className={styles.wrapper}>
+        {this.data.user ? (
+          <div>
+            <a className={styles.logout} onClick={User.logOut}>
+              <svg viewBox="0 0 60 60">
+                <path d="M0,0 L30,0 L30,10 L10,10 L10,50 L30,50 L30,60 L0,60 Z"></path>
+                <path d="M20,23 L40,23 L40,10 L60,30 L40,50 L40,37 L20,37 Z"></path>
+              </svg>
+            </a>
+            <Main />
+          </div>
+        ) : <Login />}
+      </div>
+    )
   },
 })
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,11 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './src/index'
+    './src/index',
   ],
   resolve: {
     modulesDirectories: ['src', 'node_modules'],
@@ -13,17 +13,23 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
-};
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.sass$/,
+        loader: 'style!css?modules&importLoaders=2&localIdentName=[local]___[hash:base64:5]!sass?outputStyle=expanded&imagePath=/assets/images&indentedSyntax=true&includePaths[]=' + path.resolve(__dirname, './assets/stylesheets'), // eslint-disable-line max-len
+      },
+    ],
+  },
+}
