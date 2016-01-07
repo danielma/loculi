e0cf4ff423444a9a836a72751fff6472d6d418e9
Daniel Ma <drailskid@yahoo.com>
2016-01-06 22:16:55 -0800

    pop something up on click

--- src/components/ListDesignation/ListDesignation.js
+++ src/components/ListDesignation/ListDesignation.js
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import R from 'ramda'
 import styles from '../ListDesignation/ListDesignation.sass'
 import { money } from 'utils'
 
-function payee(designationOrTransaction) {
-  return R.path(['transaction', 'payee'], designationOrTransaction) || designationOrTransaction.payee
-}
+const payee = (designationOrTransaction) => (
+  R.path(['transaction', 'payee'], designationOrTransaction) || designationOrTransaction.payee
+)
 
-export default function ListDesignation({ designation }) {
-  return (
-    <div className={styles.designation}>
-      <span className={styles.name}>{payee(designation)}</span>
-      <span className={`${styles.amount} ${designation.amountCents >= 0 ? styles.positive : styles.negative}`}>
-        {money.centsToString(designation.amountCents)}
-      </span>
-    </div>
-  )
+export default class ListDesignation extends React.Component {
+  static propTypes = {
+    designation: PropTypes.shape({
+      amountCents: PropTypes.number.isRequired,
+    }).isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isEditing: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ isEditing: !this.state.isEditing })
+  }
+
+  render() {
+    const { designation } = this.props
+
+    return (
+      <div className={styles.designation} onClick={this.handleClick}>
+        <span className={styles.name}>{payee(designation)}</span>
+        <span className={`${styles.amount} ${designation.amountCents >= 0 ? styles.positive : styles.negative}`}>
+          {money.centsToString(designation.amountCents)}
+        </span>
+        {this.state.isEditing &&
+          <div>
+            kalamaty jane
+          </div>
+        }
+      </div>
+    )
+  }
 }
