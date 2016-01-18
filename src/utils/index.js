0f87f248493ff5cc8a79d7687b919add6f98de8c
Daniel Ma <drailskid@yahoo.com>
2016-01-17 21:45:24 -0800

    esure default envelopes

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,7 @@ export money from './money'
 export string from './string'
 export react from './react'
 export ofx from './ofx'
+export ensureDefaults from './ensureDefaults'
 export function noop() {}
 
 module.exports = exports
